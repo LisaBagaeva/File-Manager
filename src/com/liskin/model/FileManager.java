2380d55e50ebc93ebbd48bab874baa4855c20528
LisaBagaeva <elisaveta.bagaeva@autodesk.com>
2016-12-18 22:26:47 +0300

    Add variable offset

--- src/com/liskin/model/FileManager.java
+++ src/com/liskin/model/FileManager.java
@@ -3,6 +3,7 @@ package com.liskin.model;
 import java.io.File;
 
 public class FileManager {
+	private static String offset;
 
 	public static void getTree(String path) {
 		if (path.equals("") || path.equals(null))
@@ -11,26 +12,24 @@ public class FileManager {
 		if (!file.exists())
 			throw new IllegalArgumentException("Illegal argument:" + path);
 		System.out.println(file.getName());
-		
-		String fileParent = new String(file.getParent());
-		getTree(file, fileParent);
+		offset = new String(new char[file.getName().length()]);
+		String defaultOffset = new String(new char[file.getName().length()]);
+		getTree(file,defaultOffset);
 	}
 
-	public static void getTree(File file, String parentFile) {
-		if (file.isDirectory()) {
-			File[] children = file.listFiles();
-			for (int i = 0; i < children.length; i++) {
-				if (children[i].isDirectory()) {
-					for (int j = 0; j < file.getPath().length() - parentFile.length(); j++)
-						System.out.print(" ");
-					System.out.print(children[i].getName() + "\n");
-					getTree(children[i], parentFile);
-				} else {
-					for (int j = 0; j < file.getPath().length() - parentFile.length(); j++)
-						System.out.print(" ");
-					System.out.print(children[i].getName()+ "\n");
-				}
+	public static void getTree(File file, String defaultOffset) {
+		if(file.isDirectory()){
+		File[] children = file.listFiles();
+		for (int i = 0; i < children.length; i++) {
+			if (children[i].isDirectory()) {
+				System.out.print(offset + children[i].getName() + "\n");
+				offset = offset.concat(" ");
+				getTree(children[i],defaultOffset);
+			} else {
+				System.out.print(offset + children[i].getName() + "\n");
+				offset = defaultOffset;
 			}
 		}
+	}
 	}
 }
--- src/com/liskin/model/Main.java
+++ src/com/liskin/model/Main.java
@@ -3,6 +3,6 @@ package com.liskin.model;
 public class Main { 
 
 public static void main(String[] args) { 
-FileManager.getTree("C:\\Users\\Home\\Desktop\\Test"); 
+FileManager.getTree("C:\\Users\\Home\\Desktop\\"); 
 } 
 }
